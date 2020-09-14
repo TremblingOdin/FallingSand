3bda80c485901d18ac68e3730757668cbc35ae64
TremblingOdin <pigmekevin@gmail.com>
2020-09-14 12:26:32 -0600

    initialized the menu

---
+++ scripts/menu.js
@@ -0,0 +1,32 @@
+/* Configuration */
+const ELEMENTS_PER_ROW = 4;
+const PEN_SIZES = [2,4,8,16,32,64];
+const PEN_SIZES_LABELS = ["1px", "2px", "4px", "8px", "16px", "32px"];
+const DEFAULT_PEN_IDX = 1;
+
+const elementMenuItems = [
+    SAND, WATER, PLANT, FIRE, SPOUT,
+    WELL, SALT, OIL, WAX, ICE, GUNPOWDER,
+    SOIL, C4, LAVA, MYSTERY, CONCRETE, 
+    BACKGROUND, BARRIER
+];
+
+const menuNames = {};
+menuNames[SAND] = "SAND";
+menuNames[WATER] = "WATER";
+menuNames[PLANT] = "PLANT";
+menuNames[FIRE] = "FIRE";
+menuNames[SPOUT] = "SPOUT";
+menuNames[WELL] = "WELL";
+menuNames[SALT] = "SALT";
+menuNames[OIL] = "OIL";
+menuNames[WAX] = "WAX";
+menuNames[ICE] = "ICE";
+menuNames[GUNPOWDER] = "GUNPOWDER";
+menuNames[SOIL] = "SOIL";
+menuNames[C4] = "C4";
+menuNames[LAVA] = "LAVA";
+menuNames[MYSTERY] = "MYSTERY";
+menuNames[CONCRETE] = "CONCRETE";
+menuNames[BACKGROUND] = "BACKGROUND";
+menuNames[BARRIER] = "BARRIER";
